84a14fa7a298be88ea4b5f673e58dd7943844d32
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-09-28 07:28:07 +0500

    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload
    https://github.com/junaidsheikh0002/PIAIC-GenAI-Classes-Empowering-Pakistan-with-Generative-AI-Skills_By_Junaid_Ul_Haque_Sheikh_Part_001/upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('button');
+    button.addEventListener('click', function() {
+        alert('You have successfully enrolled!');
+    });
+});
